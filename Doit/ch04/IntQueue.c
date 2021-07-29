eb7de8b419a7bb32e67c7146cfdf1e1dc28c0a78
윤영진 <alwns28@kookmin.ac.kr>
2021-07-29 22:38:27 +0900

    2021.07.29

---
+++ Doit/ch04/IntQueue.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"IntQueue.h"
+
+// 큐 초기화
+int Initialize(IntQueue *q, int max){
+    q->num = 0;
+    q->max = max;
+
+    if((q->que = calloc(max, sizeof(int))) == NULL){
+        puts("큐 생성에 실패하였습니다. ");
+        return -1;
+    }
+    return 1;
+}
+
+// 인큐
+int Enqueue(IntQueue *q, int x){
+    if(q->num >= q->max){
+        puts("더이상 큐에 데이터를 넣을 수 없습니다. ");
+        return -1;
+    }
+    q->que[q->num] = x;
+    q->num++;
+    return 1;
+}
+
+// 디큐
+int Dequeue(IntQueue *q){
+    int value, i;
+
+    if(q->num < 0){
+        puts("큐에 데이터가 남아있지 않습니다. ");
+        return -1;
+    
+    }
+    value = q->que[0];
+    for(i=1; i<q->num; i++){
+        q->que[i-1] = q->que[i];
+    }
+    q->que[i] = 0;
+    q->num--;
+
+    return value;
+}
+
+// 큐 프린트
+void Print(IntQueue *q){
+    int i;
+
+    for(i=0; i<q->num; i++){
+        printf("%d ", q->que[i]);
+    }
+    printf("\n");
+}
---
+++ Doit/ch04/IntQueueTest.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include"IntQueue.h"
+
+int main(){
+    IntQueue q;
+
+    if(Initialize(&q, 64) == -1){
+        return 0;
+    }
+
+    while(1){
+        int menu, input, output;
+        printf("(1) Enqueue (2) Dequeue (3) Print : ");
+        scanf("%d", &menu);
+        if(menu == 0) break;
+        switch (menu)
+        {
+        case 1:
+            printf("넣을 데이터를 입력하세요. : ");
+            scanf("%d", &input);
+            if(Enqueue(&q, input) == -1){
+                printf("\a오류 : 인큐에 실패하였습니다. \n");
+                return 0;
+            };
+            break;
+        case 2:
+            output = Dequeue(&q);
+            if(output == -1){
+                printf("\a오류 : 디큐에 실패하였습니다. \n");
+                return 0;
+            }
+            printf("디큐한 데이터는 %d 입니다.\n", output);
+            break;
+        case 3:
+            Print(&q);
+        default:
+            break;
+        }
+    }
+}
