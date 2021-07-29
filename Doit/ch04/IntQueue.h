9631f3ef076ca903b9deb3429de99ffb029df02b
윤영진 <alwns28@kookmin.ac.kr>
2021-07-29 21:39:55 +0900

    2021.07.29

---
+++ Doit/ch04/IntQueue.h
@@ -0,0 +1,23 @@
+#ifndef __IntQueue
+#define __IntQueue
+
+typedef struct 
+{
+    int max;
+    int num;
+    int *que;
+}IntQueue;
+
+// 큐 초기화
+int Initialize(IntQueue *q, int max);
+
+// 인큐
+int Enqueue(IntQueue *q, int x);
+
+// 디큐
+int Dequeue(IntQueue *q);
+
+// 큐 프린트
+void Print(IntQueue *q);
+
+#endif
