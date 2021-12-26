d185dcea8b66eb0cafe9726f58e65d022d8e426e
윤영진 <alwns28@kookmin.ac.kr>
2021-12-27 00:39:51 +0900

    항목을 벗어나도 데이터 유지하도록 수정

--- problem/DVD_reservation_program/source/main.c
+++ problem/DVD_reservation_program/source/main.c
@@ -4,25 +4,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void yj_user_mode()
+void print_logo()
 {
-    YJ_ST_MANAGE_TABLE *dvd_user_table[MAX_TABLE_SIZE];
+    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    printf("@                                                              @\n");
+    printf("@                     DVD MANAGEMENT PROGRAM                   @\n");
+    printf("@                                                              @\n");
+    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+}
 
+void yj_user_mode(YJ_ST_MANAGE_TABLE *dvd_user_table[MAX_TABLE_SIZE], int *table_count)
+{
     int select = 0;
-    int table_count = 0;
     int i;
 
-    for(i=0; i<MAX_TABLE_SIZE; i++){
-        dvd_user_table[i] = malloc(sizeof(YJ_ST_MANAGE_TABLE));
-        yj_dvd_st_init(dvd_user_table[i]);
-    }
     while(1)
     {
-        printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-        printf("@                                                              @\n");
-        printf("@                     DVD MANAGEMENT PROGRAM                   @\n");
-        printf("@                                                              @\n");
-        printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+        print_logo();
         
         printf("\n\n\t\t\t 1. Input User info\n");
         printf("\t\t\t 2. Print user info \n");
@@ -32,11 +30,11 @@ void yj_user_mode()
         switch (select)
         {
         case 1:
-            input_user_data(dvd_user_table[table_count]->user_st);
-            table_count += 1;
+            input_user_data(dvd_user_table[*table_count]->user_st);
+            *table_count += 1;
             break;
         case 2:
-            for(i=0; i<table_count; i++){
+            for(i=0; i<*table_count; i++){
                 print_user_list(dvd_user_table[i]);
             }
             break;
@@ -48,30 +46,16 @@ void yj_user_mode()
         }
     }
 USER_FIN:;
-    for(i=0; i<MAX_TABLE_SIZE; i++){
-        free(dvd_user_table[i]);
-    }
 }
 
-void yj_root_mode()
+void yj_root_mode(YJ_DVD_INFO_ST *dvd_root_table[MAX_DVD_NUM], int *table_count)
 {
-    YJ_DVD_INFO_ST *dvd_root_table[MAX_DVD_NUM];
-
     int select = 0;
-    int table_count = 0;
     int i;
 
-    for(i=0; i<MAX_DVD_NUM; i++){
-        dvd_root_table[i] = malloc(sizeof(YJ_DVD_INFO_ST));
-        yj_dvd_server_init(dvd_root_table[i]);
-    }
     while(1)
     {
-        printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-        printf("@                                                              @\n");
-        printf("@                     DVD MANAGEMENT PROGRAM                   @\n");
-        printf("@                                                              @\n");
-        printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+        print_logo();
         
         printf("\n\n\t\t\t 1. Input DVD info\n");
         printf("\t\t\t 2. Print DVD info \n");
@@ -81,11 +65,11 @@ void yj_root_mode()
         switch (select)
         {
         case 1:
-            input_dvd_data(dvd_root_table[table_count]);
-            table_count += 1;
+            input_dvd_data(dvd_root_table[*table_count]);
+            *table_count += 1;
             break;
         case 2:
-            for(i=0; i<table_count; i++){
+            for(i=0; i<*table_count; i++){
                 print_dvd_list(dvd_root_table[i]);
             }
             break;
@@ -97,21 +81,30 @@ void yj_root_mode()
         }
     }
 ROOT_FIN:;
-    for(i=0; i<MAX_TABLE_SIZE; i++){
-        free(dvd_root_table[i]);
-    }
 }  
 
 int main()
 {
     int select_main_menu;
+    int user_table_count = 0;
+    int root_table_count = 0;
+    int i;
+
+    YJ_ST_MANAGE_TABLE *dvd_user_table[MAX_TABLE_SIZE];
+    YJ_DVD_INFO_ST *dvd_root_table[MAX_DVD_NUM];
+
+    for(i=0; i<MAX_DVD_NUM; i++){
+        dvd_root_table[i] = malloc(sizeof(YJ_DVD_INFO_ST));
+        yj_dvd_server_init(dvd_root_table[i]);
+    }
+    for(i=0; i<MAX_TABLE_SIZE; i++){
+        dvd_user_table[i] = malloc(sizeof(YJ_ST_MANAGE_TABLE));
+        yj_dvd_st_init(dvd_user_table[i]);
+    }
+
     while(1)
     {
-        printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-        printf("@                                                              @\n");
-        printf("@                     DVD MANAGEMENT PROGRAM                   @\n");
-        printf("@                                                              @\n");
-        printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+        print_logo();
         
         printf("\n\n\t\t\t 1. USER MODE\n");
         printf("\t\t\t 2. ROOD MODE \n");
@@ -121,10 +114,10 @@ int main()
         switch (select_main_menu)
         {
         case 1:
-            yj_user_mode();
+            yj_user_mode(dvd_user_table, &user_table_count);
             break;
         case 2:
-            yj_root_mode();
+            yj_root_mode(dvd_root_table, &root_table_count);
             break;
         case 0:
             goto MAIN_FIN;
@@ -135,6 +128,12 @@ int main()
         }
     }
 MAIN_FIN:;
+    for(i=0; i<MAX_TABLE_SIZE; i++){
+        free(dvd_user_table[i]);
+    }
+    for(i=0; i<MAX_DVD_NUM; i++){
+        free(dvd_root_table[i]);
+    }
     return 0;
 
 }
