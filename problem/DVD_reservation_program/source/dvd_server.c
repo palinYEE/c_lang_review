108f44a98e1fd77c75dc831575885ce4ca0ac3c4
Yoon Yeoung-jin <73421059+palinYEE@users.noreply.github.com>
2021-12-28 17:14:53 +0900

    연채 확인 항목 추가중

--- problem/DVD_reservation_program/source/dvd_server.c
+++ problem/DVD_reservation_program/source/dvd_server.c
@@ -25,3 +25,23 @@ void print_dvd_list(YJ_DVD_INFO_ST *in)
     printf("\t\t\t rental date : %s\n\n", in->release_year);
     printf("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
 }
+
+void check_lotus_user(YJ_ST_MANAGE_TABLE *dvd_user_table[MAX_TABLE_SIZE], int *count)
+{
+    int i;
+    YJ_RENTAL_INFO_ST *rental_temp;
+    YJ_DVD_USER_ST *user_temp;
+
+    for(i=0; i<*count; i++){
+        rental_temp = dvd_user_table[i]->rental_info_st;
+        user_temp = dvd_user_table[i]->user_st;
+        if(rental_temp->rental_date == NULL){
+            printf(" - %s 님은 DVD를 대여하지 않았습니다. \n", user_temp->user_name);
+        }
+        else{
+            // 빌린 날짜 형식은 다음과 같다. 
+            // year/month/day ~ year/month/day+10
+            // 오늘 날짜가 해당 기간 사이에 있으면 연채 아님. 이 밖에 있으연 연채.
+        }
+    }
+}
