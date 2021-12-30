2df091ba1a8c8e01a2276d61926a3f9cd2a8b6cd
palinYEE <alwns28@kookmin.ac.kr>
2021-12-30 11:30:01 +0900

    모든 데이터를 파일로 추출하는 함수 추가

--- problem/DVD_reservation_program/source/yj_utils.c
+++ problem/DVD_reservation_program/source/yj_utils.c
@@ -20,7 +20,7 @@ void yj_setting_rental_time(YJ_RENTAL_INFO_ST *out)
     yj_time_now_int = time(NULL);
     yj_time_fin_int = yj_time_now_int + 10*24*60*60     // days * hours * minutes * second
     yj_time_now_struct = localtime(&yj_time_now_int);
-    yj_time_fin_struct = localtime(&yj_time_fin_struct);
+    yj_time_fin_struct = localtime(&yj_time_fin_int);
 
     out->rental_date = yj_time_now_struct;
     out->return_date = yj_time_fin_struct;
@@ -30,22 +30,57 @@ void yj_setting_rental_time(YJ_RENTAL_INFO_ST *out)
 
 void extraction_data_file(YJ_ST_MANAGE_TABLE *dvd_user_table[MAX_TABLE_SIZE], YJ_DVD_INFO_ST *dvd_root_table[MAX_DVD_NUM], int *user_table_count, int *root_table_count,char *file_name){
 
+    /* file value setting */
     FILE *fp;
     fp = fopen(file_name, "a");
-
     /* loop value */
     int i;
+    /* temp value */
+    YJ_DVD_USER_ST *user_temp;
+    YJ_RENTAL_INFO_ST *rental_temp;
+    /* time value setting */
+    time_t now_time_int;
+    struct tm *now_time_st;
+    now_time_int = time(NULL);
+    now_time_st = localtime(&now_time_int);
 
+    
+    fprintf(fp, "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    fprintf(fp, "@                                                              @\n");
+    fprintf(fp, "@                     DVD MANAGEMENT PROGRAM                   @\n");
+    fprintf(fp, "@                                                              @\n");
+    fprintf(fp, "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    fprintf(fp, "\n - extract date : %d-%d-%d", now_time_st->tm_year+1900, now_time_st->tm_mon ,now_time_st->tm_mday)
+    fprintf(fp, " - File name : %s", file_name);
+    fprintf(fp, "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    fprintf(fp, " -- USER DATA \n");
     for(i=0; i<*user_table_count; i++){
         // user data extraction
+        user_temp = dvd_user_table[i]->user_st;
+        rental_temp = dvd_user_table[i]->rental_info_st;
+        fprintf(fp, " - Name : %s\n", user_temp->user_name);
+        fprintf(fp, " - Phone Number : %s\n", user_temp->phone_number);
+        if(rental_temp->lotus_check != ""){
+            fprintf(fp, " - DVD Title : %s\n", rental_temp->title);
+            fprintf(fp, " - DVD Rental Date : %d-%d-%d:%d-%d\n", rental_temp->rental_date->tm_year+1900, rental_temp->rental_date->tm_mon, rental_temp->rental_date->tm_mday, rental_temp->rental_date->tm_hour, rental_temp->rental_date->tm_min);
+            fprintf(fp, " - DVD Return Date : %d-%d-%d\n", return_date->rental_date->tm_year+1900, return_date->rental_date->tm_mon, return_date->rental_date->tm_mday);
+            fprintf(fp, " - DVD Lotus Check : %s", rental_temp->lotus_check);
+        }
+        fprintf(fp, "=======================\n");
     }    
-
+    fprintf(fp, "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    fprintf(fp, " -- DVD DATA \n");
     for(i=0; i<*root_table_count; i++){
         // dvd data extraction
+        fprintf(fp, " - DVD Title : %s\n", dvd_root_table[i]->title);
+        fprintf(fp, " - DVD ISBN : %d\n", dvd_root_table[i]->ISBN)l
+        fprintf(fp, " - DVD Genre : %s\n", dvd_root_table[i]->genre);
+        fprintf(fp, " - release_year : %s\n", dvd_root_table[i]->release_year);
+        fprintf(fp, "=======================\n");
     }
 
-
     // all data terminate logic add.
-
-
+    *user_table_count = 0;
+    *root_table_count = 0;
+    fclose(fp);
 }
