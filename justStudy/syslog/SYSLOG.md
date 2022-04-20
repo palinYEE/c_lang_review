0f5cd650e1b79007f8741f2b04fca5fd35140577
palinYEE <alwns28@kookmin.ac.kr>
2022-04-20 04:56:38 +0200

    [codes] syslog example add case of removing openlog and closelog

---
+++ justStudy/syslog/SYSLOG.md
@@ -0,0 +1,9 @@
+# SYSLOG 정리
+
+## 사용 방법
+
+* `openlog -> syslog or vsyslog -> closelog` 순으로 사용
+* 
+
+## 사용 함수 원형
+
--- justStudy/syslog/yyj_syslog.c
+++ justStudy/syslog/yyj_syslog.c
@@ -2,27 +2,48 @@
 #include<signal.h>
 #include<syslog.h>
 
+void test_sig_handler_1(int signum);
+void test_sig_handler_2(int signum);
 
-void test_sig_handler(int signum){
-    printf("[YYJ SIGNAL]\n");
+#define CASE 2
+
+#if CASE == 1
+#define sig_handler test_sig_handler_1
+#elif CASE == 2
+#define sig_handler test_sig_handler_2
+#endif
+
+
+void test_sig_handler_1(int signum){
+    printf("[YYJ SIGNAL_1]\n");
     openlog("[YYJ-SYSLOG TEST]", LOG_CONS|LOG_NDELAY|LOG_PID, LOG_LOCAL0);
     syslog(LOG_EMERG, "YYJ LOG_EMERG TEST %d", signum);
     syslog(LOG_NOTICE, "YYJ LOG_NOTICE TEST %d", signum);
     closelog();
-    printf("[YYJ CLOSE SIGNAL]\n");
+    printf("[YYJ CLOSE SIGNAL_1]\n");
 
     _exit(127);
+}
 
+void test_sig_handler_2(int signum){
+    printf("[YYJ SIGNAL_2]\n");
+    // openlog("[YYJ-SYSLOG TEST]", LOG_CONS|LOG_NDELAY|LOG_PID, LOG_LOCAL0);
+    syslog(LOG_EMERG, "YYJ LOG_EMERG TEST %d", signum);
+    syslog(LOG_NOTICE, "YYJ LOG_NOTICE TEST %d", signum);
+    // closelog();
+    printf("[YYJ CLOSE SIGNAL_2]\n");
+
+    _exit(127);
 }
 
 int main(int argc, char **argv){
-    signal(SIGINT, test_sig_handler);
-    signal(SIGILL, test_sig_handler);
-    signal(SIGABRT, test_sig_handler);
-    signal(SIGBUS, test_sig_handler);
-    signal(SIGSTKFLT, test_sig_handler);
-    signal(SIGFPE, test_sig_handler);
-    signal(SIGSEGV, test_sig_handler);
+    signal(SIGINT, sig_handler);
+    signal(SIGILL, sig_handler);
+    signal(SIGABRT, sig_handler);
+    signal(SIGBUS, sig_handler);
+    signal(SIGSTKFLT, sig_handler);
+    signal(SIGFPE, sig_handler);
+    signal(SIGSEGV, sig_handler);
 
     while(1){
 	sleep(1);
